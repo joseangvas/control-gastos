d12b9ca5ea394a5af8df4bd522eaa843b37ed8fc
Joseangvas <developer.javp@gmail.com>
2024-01-10 06:43:27 -0400

    Codificar Componente NuevoPresupuesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import NuevoPresupuesto from './components/NuevoPresupuesto'
 const Header = () => {
   return (
     <header>
       <h1>Planificador de Gastos</h1>
-    
+      <NuevoPresupuesto />
+
     </header>
   )
 }
