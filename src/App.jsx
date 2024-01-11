c6b66d57b8b931b94673c9f94669de1a43750bc9
Joseangvas <developer.javp@gmail.com>
2024-01-11 09:48:12 -0400

    Corrección de Errores

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import {useState} from 'react'
 import Header from './components/Header'
 
 function App() {
-  const [presupuesto, setPresupuesto] = useState(0);
-  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  const [presupuesto, setPresupuesto] = useState(0)
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
   
   return (
     <div>
