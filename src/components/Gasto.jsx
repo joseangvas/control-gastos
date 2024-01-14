cc14e9172e334c366471190a90d75faced6c1cc4
Joseangvas <developer.javp@gmail.com>
2024-01-14 12:30:56 -0400

    Codificar componente Gasto.jsx

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 
 const Gasto = ({gasto}) => {
-  const {categoria} = gasto
+  const {categoria, nombre, cantidad, id} = gasto
 
   return (
     <div className="gasto sombra">
       <div className="contenido-gasto">
 
         <div className="descripcion-gasto">
-          <p className="categoria">
-            {categoria}
-          </p>
+          <p className="categoria">{categoria}</p>
+          <p classname="nombre-gasto">{nombre}</p>
         </div>
       </div>
     </div>
