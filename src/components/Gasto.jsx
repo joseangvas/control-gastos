9855a7516cc8e40e6c26b1088cc6af9da6565f36
Joseangvas <developer.javp@gmail.com>
2024-01-14 09:29:28 -0400

    Codificar Componente Gasto.

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -4,7 +4,10 @@ const Gasto = () => {
   return (
     <div className="gasto sombra">
       <div className="contenido-gasto">
-        
+
+        <div className="descripcion-gasto">
+          
+        </div>
       </div>
     </div>
   )
