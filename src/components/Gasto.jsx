0686247211cecdef2d70d8c141cf4d7985f38401
Joseangvas <developer.javp@gmail.com>
2024-01-14 20:07:19 -0400

    Importar Iconos de Gastos

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import {formatearFecha} from '../helpers'
 
+import IconoAhorro from '../img/icono_ahorro.svg'
+import IconoCasa from '../img/icono_casa.svg'
+import IconoComida from '../img/icono_comida.svg'
+import IconoGastos from '../img/icono_gastos.svg'
+import IconoOcio from '../img/icono_ocio.svg'
+import IconoSalud from '../img/icono_salud.svg'
+import IconoSuscripciones from '../img/icono_suscripciones.svg'
+
+const diccionarioIconos = {
+  ahorro: IconoAhorro,
+  casa: IconoCasa,
+  comida: IconoComida,
+  gastos: IconoGastos,
+  ocio: IconoOcio,
+  salud: IconoSalud,
+  suscripciones: IconoSuscripciones
+}
+
 const Gasto = ({gasto}) => {
   const {categoria, nombre, cantidad, id, fecha} = gasto
 
   return (
     <div className="gasto sombra">
       <div className="contenido-gasto">
-        {/* Imagen */}
+        <img 
+          
+
+        />
         <div className="descripcion-gasto">
           <p className="categoria">{categoria}</p>
           <p className="nombre-gasto">{nombre}</p>
