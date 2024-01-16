eb196de553c62cdb79980292f43df49b5c54d0bf
Joseangvas <developer.javp@gmail.com>
2024-01-15 22:10:26 -0400

    Codificar Editar Gasto

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -3,6 +3,7 @@ import {useState, useEffect} from 'react'
 const ControlPresupuesto = ({
   gastos,
   presupuesto,
+  setGastos,
   setPresupuesto,
   setIsValidPresupuesto,
 }) => {
--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,54 +1,58 @@
-import React from 'react'
+import React from "react";
 import {
   LeadingActions,
   SwipeableList,
   SwipeableListItem,
   SwipeAction,
-  TrailingActions
-} from 'react-swipeable-list'
-import "react-swipeable-list/dist/styles.css"
-import {formatearFecha} from '../helpers'
+  TrailingActions,
+} from "react-swipeable-list";
+import "react-swipeable-list/dist/styles.css";
 
-import IconoAhorro from '../img/icono_ahorro.svg'
-import IconoCasa from '../img/icono_casa.svg'
-import IconoComida from '../img/icono_comida.svg'
-import IconoGastos from '../img/icono_gastos.svg'
-import IconoOcio from '../img/icono_ocio.svg'
-import IconoSalud from '../img/icono_salud.svg'
-import IconoSuscripciones from '../img/icono_suscripciones.svg'
+import { formatearFecha } from "../helpers";
+
+import IconoAhorro from "../img/icono_ahorro.svg";
+import IconoCasa from "../img/icono_casa.svg";
+import IconoComida from "../img/icono_comida.svg";
+import IconoGastos from "../img/icono_gastos.svg";
+import IconoOcio from "../img/icono_ocio.svg";
+import IconoSalud from "../img/icono_salud.svg";
+import IconoSuscripciones from "../img/icono_suscripciones.svg";
 
 const diccionarioIconos = {
   ahorro: IconoAhorro,
-  casa: IconoCasa,
   comida: IconoComida,
+  casa: IconoCasa,
   gastos: IconoGastos,
   ocio: IconoOcio,
   salud: IconoSalud,
-  suscripciones: IconoSuscripciones
-}
+  suscripciones: IconoSuscripciones,
+};
 
-const Gasto = ({gasto, setGastoEditar}) => {
-  const {categoria, nombre, cantidad, id, fecha} = gasto
+const Gasto = ({ gasto, setGastoEditar }) => {
+  const { categoria, nombre, cantidad, id, fecha } = gasto;
 
   const leadingActions = () => (
     <LeadingActions>
-      <SwipeAction onClick={() => (setGastoEditar(gasto))}>
+      <SwipeAction onClick={() => setGastoEditar(gasto)}>
         Editar
       </SwipeAction>
     </LeadingActions>
-  )
+  );
 
   const TrailingActions = () => (
     <TrailingActions>
-      <SwipeAction onClick={() => ()}>
+      <SwipeAction onClick={() => console.log("Eliminar")}>
         Eliminar
       </SwipeAction>
     </TrailingActions>
-  )
+  );
 
   return (
     <SwipeableList>
-      <SwipeableListItem>
+      <SwipeableListItem
+        leadingActions={leadingActions()}
+        trailingActions={trailingActions()}
+      >
         <div className="gasto sombra">
           <div className="contenido-gasto">
             <img src={diccionarioIconos[categoria]} alt="Icono Gasto" />
@@ -66,6 +70,6 @@ const Gasto = ({gasto, setGastoEditar}) => {
       </SwipeableListItem>
     </SwipeableList>
   );
-}
+};
 
-export default Gasto
+export default Gasto;
