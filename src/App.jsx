f165f9fefdb72b5c23a2ba1c3f6a701ee16d6b3d
Joseangvas <developer.javp@gmail.com>
2024-01-24 05:59:04 -0400

    Correcciones en Componentes.

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   useEffect(() => {
     if(Object.keys(gastoEditar).length > 0) {
       setModal(true)
-
+4
       setTimeout(() => {
         setAnimarModal(true)
       }, 500)
