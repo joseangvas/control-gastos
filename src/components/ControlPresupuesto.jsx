88b75539d6f40d7faebe86563342e2dfe0e6d898
Joseangvas <developer.javp@gmail.com>
2024-02-09 20:22:10 -0400

    Configurar React Circular Progress

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react'
-import {CircularProgressbar} from "react-circular-progressbar"
+import {CircularProgressbar} from 'react-circular-progressbar'
 import "react-circular-progressbar/dist/styles.css"
 
 const ControlPresupuesto = ({
