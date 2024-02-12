7747ce42f5a853173f6b6445071f915b720d9600
Joseangvas <developer.javp@gmail.com>
2024-02-12 12:38:49 -0400

    Codificar Filtro de Gastos por Categoria

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -10,33 +10,31 @@ const ListadoGastos = ({
   }) => {
   return (
     <div className="listado-gastos contenedor">
-
-      {
-        filtro ? (
-          <>
-            <h2>{gastosFiltrados.length ? "Gastos" : "No Hay Gastos aún"}</h2>
-            {gastosFiltrados.map((gasto) => (
-              <Gasto
-                key={gasto.id}
-                gasto={gasto}
-                setGastoEditar={setGastoEditar}
-                eliminarGasto={eliminarGasto}
-              />
-            ))}
-          </>
-        ) : (
-          <>
-            {gastos.map((gasto) => (
-              <Gasto
-                key={gasto.id}
-                gasto={gasto}
-                setGastoEditar={setGastoEditar}
-                eliminarGasto={eliminarGasto}
-              />
-            ))}
-          </>
-        )
-      }
+      {filtro ? (
+        <>
+          <h2>{gastosFiltrados.length ? "Gastos" : "No Hay Gastos aún en esta Categoría"}</h2>
+          {gastosFiltrados.map((gasto) => (
+            <Gasto
+              key={gasto.id}
+              gasto={gasto}
+              setGastoEditar={setGastoEditar}
+              eliminarGasto={eliminarGasto}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2>{gastos.length ? "Gastos" : "No Hay Gastos aún"}</h2>{" "}
+          {gastos.map((gasto) => (
+            <Gasto
+              key={gasto.id}
+              gasto={gasto}
+              setGastoEditar={setGastoEditar}
+              eliminarGasto={eliminarGasto}
+            />
+          ))}
+        </>
+      )}
     </div>
   );
 };
